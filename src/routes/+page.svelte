8a078c8a87e534ae4e3e7d0ccea0008f4b19d446
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-08-21 13:56:00 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount } from "svelte";
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  let posts = [];
+
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+  
+  	posts = data; //
+  });
+
+  export let name;
+
+	
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  {#each posts as article}
+    <div>
+      <p>{article.title}</p>
+    </div>
+  {/each}
+</main>
+
+
+<style>
+  /* ommitted for brevity */
+</style>
